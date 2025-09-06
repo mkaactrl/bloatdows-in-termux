b6d434a91cab6e0c69db4f660ccfc81e1c67e9b2
mkaactrl <flyerccount41@gmail.com>
2025-09-06 02:56:34 -0400

    feat: added winver command displaying fake Windows XP version

---
+++ SOURCE/SCRIPTS/winver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# winver.sh - Displays fake Bloatdows version information
+# Part of the Bloatdows suite for Termux
+
+cat << "EOFWINVER"
+Microsoft Windows XP [Version 5.1.2600]
+(C) Copyright 1985-2001 Microsoft Corp.
+EOFWINVER
+
+echo ""
+echo "Bloatdows Terminal Environment v1.0"
+echo "Built with ❤️  and pure bloat for Termux"
+echo "C:\Termux\System32>"
