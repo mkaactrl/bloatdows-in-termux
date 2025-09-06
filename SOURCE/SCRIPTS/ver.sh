4598298632cbe828d2be4b58fa013b3fd0b4b016
mkaactrl <flyerccount41@gmail.com>
2025-09-06 03:18:20 -0400

    feat: added ver command for quick version display

---
+++ SOURCE/SCRIPTS/ver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ver.sh - Quick version display command
+# Part of the Bloatdows suite for Termux
+
+echo "Microsoft Windows XP [Version 5.1.2600]"
+echo "(C) Copyright 1985-2001 Microsoft Corp."
+echo "Bloatdows Terminal Environment v1.0"
+echo "C:\Termux\System32>"
